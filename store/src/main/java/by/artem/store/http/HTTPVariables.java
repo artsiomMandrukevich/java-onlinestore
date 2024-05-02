1f755fb94d089044ae93a581c86a53a98bc78ae5
ArtemMandrukevich <fGUy7Zihrn2Q>
2024-05-02 22:02:51 +0200

    008 Interaction through HTTP

---
+++ store/src/main/java/by/artem/store/http/HTTPVariables.java
@@ -0,0 +1,9 @@
+package by.artem.store.http;
+
+public class HTTPVariables {
+
+    public static final int PORT = 8000;
+    public static final String USER = "SCOTT";
+    public static final String PASSWORD = "TIGER";
+
+}
--- store/src/main/java/by/artem/store/middleware/CommandValues.java
+++ store/src/main/java/by/artem/store/middleware/CommandValues.java
@@ -1,9 +1,12 @@
 package by.artem.store.middleware;
 
 public enum CommandValues {
+    CATEGORY,
     SORT,
     TOP,
+    CREATE_RANDOM_PURCHASE,
     CREATE_ORDER,
     PRINT_PURCHASES,
+    CLEAR_PURCHASES,
     QUIT
 }
